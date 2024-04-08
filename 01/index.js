519ccac3a56fe1d77a8192d0c344c1c2216639aa
Miči Hrešková <hroncovam@yahoo.com>
2024-04-08 19:06:09 +0200

    Add button, clicking it displays current date and time

--- 01/index.js
+++ 01/index.js
@@ -1,4 +1,9 @@
 window.onload = () => {
     const rootElement = document.getElementById("root");
-    rootElement.innerHTML = "Hello from my first SPA";
+    const button = document.createElement("button");
+    button.innerHTML = "Click me to get current date and time";
+    button.addEventListener("click", () => {
+        button.innerHTML = new Date().toString();
+    });
+    rootElement.appendChild(button);
 }

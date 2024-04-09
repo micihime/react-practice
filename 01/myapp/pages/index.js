3759c4608c944a999c43a25f958cfaaba259bdae
Miči Hrešková <hroncovam@yahoo.com>
2024-04-09 12:05:18 +0200

    Adding comments

--- 01/myapp/pages/index.js
+++ 01/myapp/pages/index.js
@@ -1,5 +1,9 @@
+//we don't have to create a base HTML file and the JavaScript (like with the simple React app built without a toolchain)
+//don't have to create a virtual DOM 
+//don't have to explicitly render virtual DOM to the browser
+
 export default function Home() {
-  return (
+  return ( //JSX
     <div>Hello!</div>
   );
 }

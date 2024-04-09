834341192f50ef035824c7b6cf0d6903edd3636f
Miči Hrešková <hroncovam@yahoo.com>
2024-04-09 10:35:25 +0200

    Render list of numbers using React library

--- 01/index.js
+++ 01/index.js
@@ -1,11 +1,18 @@
 window.onload = () => {
     const rootElement = document.getElementById("root");
-    const ints = [1, 2, 3];
+    const root = ReactDOM.createRoot(rootElement); //reate our React virtual DOM root and assign it to a new variable
+    const ints = [1,2,3];
+    const childrenElements = [];
 
-    ints.forEach(i => {
-        let li = document.createElement("li");
-        li.innerHTML = i;
-        rootElement.appendChild(li);
-    });
+    childrenElements.push(
+        React.createElement("li", { key: ints[0] }, ints[0])
+    );
+    childrenElements.push(
+        React.createElement("li", { key: ints[1] }, ints[1])
+    );
+    childrenElements.push(
+        React.createElement("li", { key: ints[2] }, ints[2])
+    );
 
+    root.render(childrenElements);
 }

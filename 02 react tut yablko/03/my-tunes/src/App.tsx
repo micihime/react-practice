278ad5fd4089dbbcafbc4707665c0f1f99349d75
Miči Hrešková <hroncovam@yahoo.com>
2024-05-19 22:39:53 +0200

    Creating link to Homepage

--- 02 react tut yablko/03/my-tunes/src/App.tsx
+++ 02 react tut yablko/03/my-tunes/src/App.tsx
@@ -1,43 +1,22 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
+
 import logo from './assets/logo.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Dude></Dude>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+        </nav>
       </header>
-      <Guy />
+      <main>
+        <img src={logo} className='App-logo' alt='logo' />
+      </main>
     </div>
   );
-}
-
-function Dude() {
-  return (
-    <div>
-      <h1>Dude</h1>
-      <p>Test test test.</p>
-    </div>
-  ) 
-}
-
-class Guy extends React.Component {
-  render() {
-    return <p>Another test.</p>
-  }
 }
 
 export default App;

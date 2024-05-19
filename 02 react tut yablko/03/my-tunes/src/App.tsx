d2f91acf7cb629eca68a9a4dcbedf84ceaebbe8a
Miči Hrešková <hroncovam@yahoo.com>
2024-05-19 22:57:18 +0200

    Adding routes and their headings

--- 02 react tut yablko/03/my-tunes/src/App.tsx
+++ 02 react tut yablko/03/my-tunes/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import { Routes, Route, NavLink } from 'react-router-dom';
 
 import logo from './assets/logo.svg';
 import './App.scss';
@@ -14,9 +14,16 @@ function App() {
           <NavLink to="/about">About</NavLink>
         </nav>
       </header>
-      <main>
-        <img src={logo} className='App-logo' alt='logo' />
+      <main className='content'>
+        <Routes>
+          <Route path="/" element={<h1>Homepage</h1>} />
+          <Route path="/tunes" element={<h1>Tunes</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+        </Routes>
       </main>
+      <footer>
+        <img src={logo} className='App-logo' alt='logo' />
+      </footer>
     </div>
   );
 }

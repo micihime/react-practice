69661cf8081fbb64676f2a01c59db0cfc8e72aec
Miči Hrešková <hroncovam@yahoo.com>
2024-05-21 16:03:38 +0100

    Update About.tsx

--- 02 react tut yablko/03/my-tunes/src/views/About.tsx
+++ 02 react tut yablko/03/my-tunes/src/views/About.tsx
@@ -5,10 +5,25 @@ import './About.scss'
 
 const About: React.FC = () => {
     return (
-        <div className='about'>
-            <h1>About</h1>
-            <p>test text test</p>
-        </div>
+        <article className="about">
+			<h1>This is an about page</h1>
+
+			<p>
+                Cupcake ipsum dolor sit amet marshmallow shortbread. Cake candy jelly-o ice cream jelly beans. Candy canes apple pie marshmallow cheesecake croissant sugar plum. Sweet tootsie roll halvah sweet brownie bonbon soufflé chocolate cake lemon drops. Cake jelly-o danish bonbon jujubes fruitcake brownie. Chocolate jelly gummies topping cupcake gummies halvah biscuit gingerbread. Cotton candy jelly-o pie jujubes liquorice pie.
+			</p>
+
+			<p>
+                Pudding chupa chups brownie bonbon candy canes. Biscuit cheesecake sesame snaps chocolate tootsie roll shortbread donut soufflé. Cake candy canes liquorice pudding chocolate.
+			</p>
+
+			<p>
+                Gingerbread pastry toffee oat cake carrot cake soufflé pudding biscuit gummi bears. Cookie fruitcake shortbread cake tiramisu icing jelly-o. Tootsie roll cake icing wafer macaroon. Gummies pastry caramels sugar plum lemon drops brownie. Shortbread jujubes fruitcake tart bear claw cookie chocolate sugar plum jujubes.
+			</p>
+
+			<p>
+                Gummies carrot cake wafer marshmallow bonbon tiramisu powder sweet roll lollipop. Pastry topping tart muffin cake biscuit. Toffee apple pie bear claw candy canes cake pudding shortbread tart. Cotton candy gummies muffin pastry tart muffin liquorice. Cookie fruitcake danish chocolate bar lemon drops. Carrot cake cookie danish brownie bonbon candy canes lemon drops gummies marzipan. Chocolate lemon drops chocolate bar marzipan candy.
+			</p>
+		</article>
     )
 }
 

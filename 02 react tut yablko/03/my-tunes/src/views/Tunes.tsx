25e9dddedaf309e94f58aa0968ed1b54c12c6162
Miči Hrešková <hroncovam@yahoo.com>
2024-05-21 18:23:05 +0100

    Adding dummy array of songs

--- 02 react tut yablko/03/my-tunes/src/views/Tunes.tsx
+++ 02 react tut yablko/03/my-tunes/src/views/Tunes.tsx
@@ -5,6 +5,23 @@ import './Tunes.scss'
 
 const Tunes: React.FC = () => {
     const [query, setQuery] = useState('')
+    const [songs, setSongs] = useState([
+        {
+            id: 1,
+            artist: 'great artist',
+            name: 'hell yeah'
+        },
+        {
+            id: 2,
+            artist: 'soul',
+            name: 'soul song'
+        },
+        {
+            id: 3,
+            artist: 'ix123',
+            name: 'the best song'
+        }
+    ])
 
     const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         setQuery(e.target.value)
@@ -13,9 +30,11 @@ const Tunes: React.FC = () => {
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         //after submit, the default action is refresh - which I do not want
         e.preventDefault()
+
         alert(query)
     }
 
+    //template
     return (
         <div className='tunes'>
             <h1>Tunes</h1>

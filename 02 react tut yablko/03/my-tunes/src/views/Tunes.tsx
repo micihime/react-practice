5bbeda9af0abdd6f1a47f25c9ce2d3cd0d7198d4
Miči Hrešková <hroncovam@yahoo.com>
2024-05-21 18:18:09 +0100

    Alert the query value on submit

--- 02 react tut yablko/03/my-tunes/src/views/Tunes.tsx
+++ 02 react tut yablko/03/my-tunes/src/views/Tunes.tsx
@@ -10,15 +10,19 @@ const Tunes: React.FC = () => {
         setQuery(e.target.value)
     }
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        //after submit, the default action is refresh - which I do not want
+        e.preventDefault()
+        alert(query)
+    }
+
     return (
         <div className='tunes'>
             <h1>Tunes</h1>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" value={query} onChange={handleInput}/>
             </form>
-            
-            <p>{query}</p>
         </div>
     )
 }

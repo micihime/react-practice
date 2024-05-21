38963e6b57a7f90de1a389db58eb822d02655dc0
Miči Hrešková <hroncovam@yahoo.com>
2024-05-21 21:46:35 +0100

    Add files for subcomponents of Tunes

---
+++ 02 react tut yablko/03/my-tunes/src/components/tunes/TunesList.tsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+
+const TunesList: React.FC = () => {
+    //template
+    return (
+        <p></p>   
+    )
+}
+
+export default TunesList
---
+++ 02 react tut yablko/03/my-tunes/src/components/tunes/TunesSearchForm.tsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+
+const TunesSearchForm: React.FC = () => {
+    //template
+    return (
+        <p></p>   
+    )
+}
+
+export default TunesSearchForm

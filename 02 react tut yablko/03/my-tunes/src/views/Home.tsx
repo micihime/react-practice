561ff45cb7e3c6db54c89c4d7c541c39ab68e1f3
Miči Hrešková <hroncovam@yahoo.com>
2024-05-21 17:42:14 +0100

    Refactoring - different way of doing the same

--- 02 react tut yablko/03/my-tunes/src/views/Home.tsx
+++ 02 react tut yablko/03/my-tunes/src/views/Home.tsx
@@ -6,15 +6,13 @@ import logo from '../assets/logo.svg';
 
 const Home: React.FC = () => {
     const [title, setTitle] = useState('React, wheee')
-    const [count, setCount] = useState(1)
 
     return (
         <div className="home">
 			<img className="logo" src={logo} alt="react logo" />
 
 			<h1 onClick={() => {
-                setCount(count + 1)
-                setTitle('React, wheee' + '!'.repeat(count))
+                setTitle(prevTitle => prevTitle + '!')
             }}>{title}</h1>
 
 			<p>

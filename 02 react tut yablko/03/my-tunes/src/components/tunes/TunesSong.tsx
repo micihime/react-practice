7f50384549d62de04c7312ab65823fd2bf13d6c3
Miči Hrešková <hroncovam@yahoo.com>
2024-05-24 13:31:25 +0100

    Limit the song title and album title length

--- 02 react tut yablko/03/my-tunes/src/components/tunes/TunesSong.tsx
+++ 02 react tut yablko/03/my-tunes/src/components/tunes/TunesSong.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Song } from '../../types'
+import { truncate } from 'lodash-es'
 
 //styles
 import './TunesSong.scss'
-import { Song } from '../../types'
 
 //props
 interface Props {
@@ -13,10 +14,19 @@ interface Props {
 const TunesSong: React.FC<Props> = (props) => {
     const {song} = props
 
+    const songify = (song: Song): string => {
+        const newTitle = song.artist + ' - ' + song.title
+        return shorten(newTitle, 100)
+    }
+
+    const shorten = (str: string, len = 55): string => {
+        return truncate(str, {length: len})
+    }
+
     //template
     return <article className='song'>
         <div className='inside'>
-            <h2>{song.artist + ' - ' + song.title}</h2>
+            <h2>{songify(song)}</h2>
             <div className="player">
                 {song.artwork && (
                     <img src={song.artwork} alt="album art"></img>
@@ -24,7 +34,7 @@ const TunesSong: React.FC<Props> = (props) => {
                 <audio controls src={song.audioFile}></audio>
             </div>
         </div>
-        <footer className='meta'>{song.album}</footer>
+        <footer className='meta'>{shorten(song.album)}</footer>
     </article>
 }
 

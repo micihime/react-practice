2e82673a74761540e595d194cfea511620d4be3f
Miči Hrešková <hroncovam@yahoo.com>
2024-05-24 13:05:46 +0100

    Adding new component for songs

---
+++ 02 react tut yablko/03/my-tunes/src/components/tunes/TunesSong.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//props
+interface Props {}
+
+//component
+const TuneSong: React.FC<Props> = () => {
+    //template
+    return <div></div>
+}
+
+export default TuneSong

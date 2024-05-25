83fac1ff284b4edcb08fa6bb724ec84fa1a76077
Miči Hrešková <hroncovam@yahoo.com>
2024-05-25 20:05:35 +0100

    Removing unnecessary files, adding some introductory text to the page

--- 02 react tut yablko/pokedex/src/App.tsx
+++ 02 react tut yablko/pokedex/src/App.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          This is React app for learning purposes, leveraging PokeAPI.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://pokeapi.co/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out PokeAPI
         </a>
       </header>
     </div>

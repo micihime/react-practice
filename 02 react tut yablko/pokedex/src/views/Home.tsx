9ee85fedfb3337f56ceb488c8e43d855f6874e5e
Miči Hrešková <hroncovam@yahoo.com>
2024-05-26 12:29:37 +0100

    Adding component views (for the navigation to display)

---
+++ 02 react tut yablko/pokedex/src/views/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>This is Home.</p>
+        </div>
+    )
+}
---
+++ 02 react tut yablko/pokedex/src/views/Pokemon.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Pokemon: React.FC = () => {
+    return (
+        <div>
+            <h1>Pokemon</h1>
+            <p>This is Pokemon section.</p>
+        </div>
+    )
+}
+
+export default Pokemon
